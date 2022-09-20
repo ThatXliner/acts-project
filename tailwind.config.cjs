a55e82be0b8b1e12577900208514af5ee54e18ee
EvanZhouDev <evanzhouyifan@gmail.com>
2022-09-19 20:18:54 -0700

    Implement Vite structure

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./*.{html,js}"],
+	theme: {
+		extend: {},
+	},
+	daisyui: {
+		themes: [
+			{
+				actsTheme: {
+					primary: "#C0E8F9",
+					secondary: "#EFC7E5",
+					accent: "#EF959C",
+					neutral: "#0D1F2D",
+					"base-100": "#FFFFFF",
+					info: "#87B2F8",
+					success: "#43D0A1",
+					warning: "#DB8906",
+					error: "#DE215D",
+				},
+			},
+		],
+	},
+	plugins: [require("daisyui")],
+};
