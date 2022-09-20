1362db2c4d4f9c7416b68675e181a270d0e512e4
EvanZhouDev <evanzhouyifan@gmail.com>
2022-09-19 20:18:43 -0700

    Removed Pirating Script

--- download_plugins.sh
+++
@@ -1,8 +0,0 @@
-urls="https://assets.codepen.io/16327/DrawSVGPlugin3.min.js
-https://assets.codepen.io/16327/MorphSVGPlugin3.min.js
-https://assets.codepen.io/16327/ScrollSmoother.min.js"
-
-for pluginUrl in $urls
-do
-    curl $pluginUrl | python pirate_plugin.py > $(basename $pluginUrl)
-done
