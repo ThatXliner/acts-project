b4946b49ed5d38490e42fbca966c409cd8c61df1
Bryan Hu <bryan.hu.2020@gmail.com>
2022-09-25 22:13:34 -0700

    formatted everything

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Acts Project
+
 A project about the Book of Acts from the Bible.
 
 If you see nothing meaningful on this page, then that's probably because there is nothing meaningful.
--- src/acts-api.js
+++ src/acts-api.js
@@ -2,40 +2,40 @@
 const SID_MAP = Object.fromEntries($store.pages.map((x, i) => [x.sID, i]));
 
 export function switchPage(sID) {
-	if (!sID in SID_MAP) {
-		console.error("Please input valid sID to switch to. Action aborted.");
-		return;
-	}
-	let currentPageNum = JSON.parse(JSON.stringify(SID_MAP[sID]));
+  if (!sID in SID_MAP) {
+    console.error("Please input valid sID to switch to. Action aborted.");
+    return;
+  }
+  let currentPageNum = JSON.parse(JSON.stringify(SID_MAP[sID]));
 
-	fetch("pages/" + sID + ".html")
-		.then((response) => response.text())
-		.then((data) => {
-			// change "page" contents
-			document.getElementById("main").innerHTML = data;
-			// modify sidebar
-			document
-				.getElementById("sidebarUl")
-				.children[$store.currentPage].classList.remove("btn-active");
-			document
-				.getElementById("sidebarUl")
-				.children[currentPageNum].classList.add("btn-active");
-                        $store.currentPage = currentPageNum;
-		        return fetch("scripts/" + sID + ".js")
-		})
-		.then((response) => response.text())
-		.then((data) => {
-			// Safer than eval; also faster.
-			Function(data)();
-		});
+  fetch("pages/" + sID + ".html")
+    .then((response) => response.text())
+    .then((data) => {
+      // change "page" contents
+      document.getElementById("main").innerHTML = data;
+      // modify sidebar
+      document
+        .getElementById("sidebarUl")
+        .children[$store.currentPage].classList.remove("btn-active");
+      document
+        .getElementById("sidebarUl")
+        .children[currentPageNum].classList.add("btn-active");
+      $store.currentPage = currentPageNum;
+      return fetch("scripts/" + sID + ".js");
+    })
+    .then((response) => response.text())
+    .then((data) => {
+      // Safer than eval; also faster.
+      Function(data)();
+    });
 }
 
 export function parseSID(sID) {
-	let finishedSID = sID.slice(4, sID.length);
-	if (finishedSID.length == 1) {
-		return "Acts " + finishedSID;
-	} else {
-		let startEnd = finishedSID.split("_");
-		return "Acts " + startEnd[0] + "-" + startEnd[1];
-	}
+  let finishedSID = sID.slice(4, sID.length);
+  if (finishedSID.length == 1) {
+    return "Acts " + finishedSID;
+  } else {
+    let startEnd = finishedSID.split("_");
+    return "Acts " + startEnd[0] + "-" + startEnd[1];
+  }
 }
