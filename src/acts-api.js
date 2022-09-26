49c02d5eabfe6bc894227a5d4ee9fc883d624c7e
EvanZhouDev <evanzhouyifan@gmail.com>
2022-09-25 21:56:33 -0700

    🐛 Fixed active drawer item bug

--- src/acts-api.js
+++ src/acts-api.js
@@ -2,40 +2,42 @@
 const SID_MAP = Object.fromEntries($store.pages.map((x, i) => [x.sID, i]));
 
 export function switchPage(sID) {
-  if (!sID in SID_MAP) {
-    console.error("Please input valid sID to switch to. Action aborted.");
-    return;
-  }
-  let currentPageNum = SID_MAP[sID];
+	if (!sID in SID_MAP) {
+		console.error("Please input valid sID to switch to. Action aborted.");
+		return;
+	}
+	let currentPageNum = JSON.parse(JSON.stringify(SID_MAP[sID]));
 
-  fetch("pages/" + sID + ".html")
-    .then((response) => response.text())
-    .then((data) => {
-      // change "page" contents
-      document.getElementById("main").innerHTML = data;
-      // modify sidebar
-      document
-        .getElementById("sidebarUl")
-        .children[$store.currentPage].classList.remove("btn-active");
-      document
-        .getElementById("sidebarUl")
-        .children[currentPageNum].classList.add("btn-active");
-    });
-  fetch("scripts/" + sID + ".js")
-    .then((response) => response.text())
-    .then((data) => {
-      // Safer than eval; also faster.
-      Function(data)();
-    });
-  $store.currentPage = currentPageNum;
+	fetch("pages/" + sID + ".html")
+		.then((response) => response.text())
+		.then((data) => {
+			// change "page" contents
+			document.getElementById("main").innerHTML = data;
+			// modify sidebar
+      console.log($store.currentPage)
+      console.log(currentPageNum)
+			document
+				.getElementById("sidebarUl")
+				.children[$store.currentPage].classList.remove("btn-active");
+			document
+				.getElementById("sidebarUl")
+				.children[currentPageNum].classList.add("btn-active");
+      $store.currentPage = currentPageNum;
+		});
+	fetch("scripts/" + sID + ".js")
+		.then((response) => response.text())
+		.then((data) => {
+			// Safer than eval; also faster.
+			Function(data)();
+		});
 }
 
 export function parseSID(sID) {
-  let finishedSID = sID.slice(4, sID.length);
-  if (finishedSID.length == 1) {
-    return "Acts " + finishedSID;
-  } else {
-    let startEnd = finishedSID.split("_");
-    return "Acts " + startEnd[0] + "-" + startEnd[1];
-  }
+	let finishedSID = sID.slice(4, sID.length);
+	if (finishedSID.length == 1) {
+		return "Acts " + finishedSID;
+	} else {
+		let startEnd = finishedSID.split("_");
+		return "Acts " + startEnd[0] + "-" + startEnd[1];
+	}
 }
