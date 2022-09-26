972a208483349006f1231bf5fab64d2f83bb94d7
Bryan Hu <bryan.hu.2020@gmail.com>
2022-09-25 17:23:13 -0700

    :art: cleaned up code

--- src/setup.js
+++ src/setup.js
@@ -1,58 +1,52 @@
 let mainJSON = {
-	currentPage: 0,
-	pages: [
-		{
-			title: "The Ascension",
-			sID: "acts1_3",
-		},
-		{
-			title: "A New Leader",
-			sID: "acts6",
-		},
-		{
-			title: "Saul's Conversion",
-			sID: "acts7_9",
-		},
-		{
-			title: "The Church",
-			sID: "acts10_15",
-		},
-		{
-			title: "Paul's Travels",
-			sID: "acts16_21",
-		},
-		{
-			title: "Paul's Arrest",
-			sID: "acts22_25",
-		},
-		{
-			title: "The End",
-			sID: "acts26_27",
-		},
-	],
+  currentPage: 0,
+  pages: [
+    {
+      title: "The Ascension",
+      sID: "acts1_3",
+    },
+    {
+      title: "A New Leader",
+      sID: "acts6",
+    },
+    {
+      title: "Saul's Conversion",
+      sID: "acts7_9",
+    },
+    {
+      title: "The Church",
+      sID: "acts10_15",
+    },
+    {
+      title: "Paul's Travels",
+      sID: "acts16_21",
+    },
+    {
+      title: "Paul's Arrest",
+      sID: "acts22_25",
+    },
+    {
+      title: "The End",
+      sID: "acts26_27",
+    },
+  ],
 };
-
+/// For the SPA
 let sIDarr = [];
 for (let i = 0; i < mainJSON.pages.length; i++) {
-	sIDarr.push(mainJSON.pages[i].sID);
+  sIDarr.push(mainJSON.pages[i].sID);
 }
 let sidebarContent = "";
 for (let i = 0; i < mainJSON.pages.length; i++) {
-	sidebarContent =
-		sidebarContent +
-		"<li><a goto=" +
-		mainJSON.pages[i].sID +
-		"><b>" +
-		parseSID(mainJSON.pages[i].sID) +
-		":</b> " +
-		mainJSON.pages[i].title +
-		"</a></li>";
+  sidebarContent += `<li><a goto=${mainJSON.pages[i].sID}><b>${parseSID(
+    mainJSON.pages[i].sID
+  )}:</b> ${mainJSON.pages[i].title}</a></li>`;
 }
 document.getElementById("sidebarUl").innerHTML = sidebarContent;
 
 document.querySelectorAll("[goto]").forEach(function (el) {
-	el.onclick = () => {
-		switchPage(el.getAttribute("goto"));
-	};
-	console.log(el.onclick);
+  el.onclick = (event) => {
+    event.preventDefault();
+    switchPage(el.getAttribute("goto"));
+  };
 });
