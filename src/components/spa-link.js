1bcf00ba942196e2f1c9f8cc3746df9747b132fc
Bryan Hu <bryan.hu.2020@gmail.com>
2022-09-30 22:48:06 -0700

    :memo: Added documentation

--- src/components/spa-link.js
+++ src/components/spa-link.js
@@ -1,5 +1,12 @@
 import { switchPage, parseSID } from "../acts-api.js";
-
+/**
+ * A web component encapsulating all the SPA
+ * event handlers, etc. If you have *any elements
+ * in it*, it *will not* dynamically change it's text
+ *
+ * @element spa-link
+ * @prop {String} to - The sID of the page you want to switch to
+ */
 export class SpaLink extends HTMLElement {
   static get observedAttributes() {
     return ["to"];
