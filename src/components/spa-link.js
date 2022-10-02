dcdcf778d38e9cb1f9e9041c0e7f160e858f00a6
EvanZhouDev <evanzhouyifan@gmail.com>
2022-10-01 20:16:42 -0700

    🎨 Formatted some stuff w/ Prettier

--- src/components/spa-link.js
+++ src/components/spa-link.js
@@ -8,33 +8,33 @@ import { switchPage, parseSID } from "../acts-api.js";
  * @prop {String} to - The sID of the page you want to switch to
  */
 export class SpaLink extends HTMLElement {
-	static get observedAttributes() {
-		return ["to"];
-	}
-	render() {
-		if (
-			this.dynamic &&
-			this.getAttribute("to") !== null &&
-			this.getAttribute("to") !== this.textContent
-		) {
-			this.textContent = parseSID(this.getAttribute("to"));
-		}
-	}
-	constructor() {
-		super();
-		this.dynamic = !this.innerHTML;
-		this.render();
-		this.onclick = (event) => {
-			event.preventDefault();
-			if (this.getAttribute("to") !== $store.pages[$store.currentPage].sID) {
-				switchPage(this.getAttribute("to"));
-			}
-		};
-	}
-	attributeChangedCallback() {
-		this.render();
-	}
+  static get observedAttributes() {
+    return ["to"];
+  }
+  render() {
+    if (
+      this.dynamic &&
+      this.getAttribute("to") !== null &&
+      this.getAttribute("to") !== this.textContent
+    ) {
+      this.textContent = parseSID(this.getAttribute("to"));
+    }
+  }
+  constructor() {
+    super();
+    this.dynamic = !this.innerHTML;
+    this.render();
+    this.onclick = (event) => {
+      event.preventDefault();
+      if (this.getAttribute("to") !== $store.pages[$store.currentPage].sID) {
+        switchPage(this.getAttribute("to"));
+      }
+    };
+  }
+  attributeChangedCallback() {
+    this.render();
+  }
 }
 if ("customElements" in window) {
-	window.customElements.define("spa-link", SpaLink);
+  window.customElements.define("spa-link", SpaLink);
 }
