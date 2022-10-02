14f50ad921f6f988329ef128aa13ae767d9c41bc
EvanZhouDev <evanzhouyifan@gmail.com>
2022-10-01 19:23:55 -0700

    🐛 Fix constructor bug

--- src/components/spa-link.js
+++ src/components/spa-link.js
@@ -1,14 +1,14 @@
-import { switchPage } from '../acts-api.js';
+import { switchPage } from "../acts-api.js";
 
-export class SpaLink extends HTMLParagraphElement {
-  constructor() {
-    super();
-    this.innerHTML = `<a>${this.innerHTML}</a>`
-    this.querySelector("a").onclick = (event) => {
-      event.preventDefault();
-      switchPage(this.getAttribute('to'));
-    }
-  }
+export class SpaLink extends HTMLElement {
+	constructor() {
+		super();
+		this.innerHTML = `<a>${this.innerHTML}</a>`;
+		this.querySelector("a").onclick = (event) => {
+			event.preventDefault();
+			switchPage(this.getAttribute("to"));
+		};
+	}
 }
 
-window.customElements.define('spa-link', SpaLink);
+window.customElements.define("spa-link", SpaLink);
