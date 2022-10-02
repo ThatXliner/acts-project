23835ac8fa9274726c1331b328d5169cd1d6a606
EvanZhouDev <evanzhouyifan@gmail.com>
2022-10-01 19:58:16 -0700

    🐛 Page no longer switches if already on target page

--- src/components/spa-link.js
+++ src/components/spa-link.js
@@ -8,31 +8,34 @@ import { switchPage, parseSID } from "../acts-api.js";
  * @prop {String} to - The sID of the page you want to switch to
  */
 export class SpaLink extends HTMLElement {
-  static get observedAttributes() {
-    return ["to"];
-  }
-  render() {
-    if (
-      this.dynamic &&
-      this.getAttribute("to") !== null &&
-      this.getAttribute("to") !== this.textContent
-    ) {
-      this.textContent = parseSID(this.getAttribute("to"));
-    }
-  }
-  constructor() {
-    super();
-    this.dynamic = !this.innerHTML;
-    this.render();
-    this.onclick = (event) => {
-      event.preventDefault();
-      switchPage(this.getAttribute("to"));
-    };
-  }
-  attributeChangedCallback() {
-    this.render();
-  }
+	static get observedAttributes() {
+		return ["to"];
+	}
+	render() {
+		if (
+			this.dynamic &&
+			this.getAttribute("to") !== null &&
+			this.getAttribute("to") !== this.textContent
+		) {
+			this.textContent = parseSID(this.getAttribute("to"));
+		}
+	}
+	constructor() {
+		super();
+		this.dynamic = !this.innerHTML;
+		this.render();
+		this.onclick = (event) => {
+			event.preventDefault();
+			if (this.getAttribute("to") !== $store.pages[$store.currentPage].sID) {
+				console.log("hi");
+				switchPage(this.getAttribute("to"));
+			}
+		};
+	}
+	attributeChangedCallback() {
+		this.render();
+	}
 }
 if ("customElements" in window) {
-  window.customElements.define("spa-link", SpaLink);
+	window.customElements.define("spa-link", SpaLink);
 }
