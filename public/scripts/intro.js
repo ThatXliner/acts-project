9299cd2b2a66c8abbfe586f868c95f7be9225053
EvanZhouDev <evanzhouyifan@gmail.com>
2022-10-02 21:20:26 -0700

    💫  Added some first animations!

--- public/scripts/intro.js
+++ public/scripts/intro.js
@@ -1,1 +1,3 @@
-gsap.to("main > div", { x: 250 });
+gsap.from("main > #introducing", { y: -200, alpha: 0 });
+gsap.from("main > #logo", { x: -200, alpha: 0, duration: 1 });
+gsap.from("main > #scrollDown", { alpha: 0, duration: 2 });
