aa29a1582b2a43d2d8907f95432908437e960bb5
EvanZhouDev <evanzhouyifan@gmail.com>
2022-10-02 21:11:24 -0700

    💄 Added public pages/scripts into tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: [
+    "./*.{html,js}",
+    "./public/pages/*.{html,js}",
+    "./public/scripts/*.{html,js}",
+  ],
   theme: {
     extend: {},
   },
