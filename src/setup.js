a622d282906a75791c97481ae32bdabb76b1fd6f
Bryan Hu <bryan.hu.2020@gmail.com>
2022-10-12 15:32:28 -0700

    :bug: Fixed images

--- src/setup.js
+++ src/setup.js
@@ -2,7 +2,7 @@ import { parseSID, switchPage } from "./acts-api.js";
 // set up side bar
 document.getElementById("sidebarUl").innerHTML = $store.pages
   .map((x) => {
-    return `<li><spa-link to=${x.sID}><b>${parseSID(x.sID)}:</b> ${
+    return `<li><spa-link to="${x.sID}"><b>${parseSID(x.sID)}:</b> ${
       x.title
     }</spa-link></li>`;
   })
