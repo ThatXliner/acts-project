11139971c823d9c4091ee36d2b160254dc33be9d
ProjectCodeAI <discordpydev@gmail.com>
2022-10-14 10:20:21 -0700

    Cleaning up

--- public/scripts/acts1_5.js
+++ public/scripts/acts1_5.js
@@ -1,54 +1,46 @@
-gsap.registerPlugin(ScrollTrigger); 
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.to('.disciples', {
-    scrollTrigger: {
-        scroller: ".drawer-content",
-        trigger: '.disciples',
-        scrub:true,
-        markers:true,
-        pin: true,
-        start: "30% 75%"
-  
-    },
-    x:40,
-    rotation: 35,
-    ease: "none",
-    duration: 2
-  })
+gsap.to(".disciples", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".disciples",
+    scrub: true,
+    markers: true,
+    pin: true,
+    start: "30% 75%",
+  },
+  x: 40,
+  rotation: 35,
+  ease: "none",
+  duration: 2,
+});
 
+gsap.to(".angel1", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".angel1",
+    scrub: 1,
+    markers: true,
+    pin: true,
+    start: "5% 15%",
+  },
+  x: 400,
+  rotation: 350,
+  ease: "none",
+  duration: 2,
+});
 
-  gsap.to('.angel1', {
-    scrollTrigger: {
-        scroller: ".drawer-content",
-        trigger: '.angel1',
-        scrub:1,
-        markers:true,
-        pin: true,
-        start: "5% 15%"
-  
-    },
-    x:400,
-    rotation: 350,
-    ease: "none",
-    duration: 2
-  })
-
-
-  gsap.to('.angel2', {
-    scrollTrigger: {
-        scroller: ".drawer-content",
-        trigger: '.angel2',
-        scrub:1,
-        markers:true,
-        pin: true,
-        start: "5% 15%"
-  
-    },
-    x:-400,
-    rotation: -350,
-    ease: "none",
-    duration: 2
-  })
-
-
-  
+gsap.to(".angel2", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".angel2",
+    scrub: 1,
+    markers: true,
+    pin: true,
+    start: "5% 15%",
+  },
+  x: -400,
+  rotation: -350,
+  ease: "none",
+  duration: 2,
+});
