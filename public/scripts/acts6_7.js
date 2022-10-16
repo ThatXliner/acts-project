bf18f88c1d85924dec2681b35f8653b6c8fcd040
Bryan Hu <bryan.hu.2020@gmail.com>
2022-10-16 16:44:40 -0700

    Initial attempt (in progress)

--- public/scripts/acts6_7.js
+++ public/scripts/acts6_7.js
@@ -1,1 +1,23 @@
-gsap.to("main > div", { x: 250 });
+// gsap.to("main > p", {
+//   //   scrollTrigger: {
+//   //     scroller: ".drawer-content",
+//   //     trigger: "main > p",
+//   //     // start: "top top",
+//   //     // pin: true,
+//   //   },
+//   motionPath: { path: "main>svg>path" },
+//   //   x: 100,
+//   duration: 1,
+// });
+gsap.to("main > p", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: "main > p",
+    // start: "top 0",
+    // end: "+=100px",
+    scrub: 1,
+    pin: true,
+    markers: true,
+  },
+  motionPath: { path: "main>svg>path" },
+});
