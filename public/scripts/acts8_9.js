848adc18003e5dd77e3f8124cbb261c21b90877e
EvanZhouDev <evanzhouyifan@gmail.com>
2022-10-17 14:54:27 -0700

    💫 Added some first animations

--- public/scripts/acts8_9.js
+++ public/scripts/acts8_9.js
@@ -1,1 +1,57 @@
-gsap.to("main > div", { x: 250 });
+gsap.from("#titles", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: "#titles",
+    pin: "#titles",
+    start: "-50px 0",
+    end: "1200px start",
+  },
+});
+gsap.to("#scene1", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: "#scene1",
+    pin: "#scene1",
+    start: "-50px 0",
+    end: "1200px start",
+    pinSpacing: "false",
+  },
+});
+gsap.from(".dove", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".dove",
+    // markers: true,
+    scrub: true,
+    start: "50px 0",
+    end: "200px start",
+  },
+  alpha: 0,
+  x: -400,
+});
+gsap.from(".simon", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".dove",
+    markers: true,
+    scrub: true,
+    start: "200px 0",
+    end: "500px start",
+  },
+//   immediateRender: false,
+  alpha: 0,
+  y: 200,
+});
+gsap.to(".simon", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".simon",
+    markers: true,
+    scrub: true,
+    start: "500px 0",
+    end: "600px start",
+  },
+  immediateRender: false,
+  y: 200,
+  alpha: 0,
+});
