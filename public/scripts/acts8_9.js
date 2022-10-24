f0c8c1314c444f058391c92a8de14fb9319779c8
EvanZhouDev <evanzhouyifan@gmail.com>
2022-10-23 17:22:57 -0700

    💫 Basically finished section.

--- public/scripts/acts8_9.js
+++ public/scripts/acts8_9.js
@@ -25,26 +25,24 @@ gsap.to("#scene2", {
     start: "-50px 0",
     end: "1200px start",
     pinSpacing: "false",
-    markers: true,
   },
 });
 gsap.from(".dove", {
   scrollTrigger: {
     scroller: ".drawer-content",
     trigger: ".dove",
-    // markers: true,
     scrub: true,
     start: "50px 0",
     end: "200px start",
   },
   alpha: 0,
   x: -400,
+  ease: "in",
 });
 gsap.from(".simon", {
   scrollTrigger: {
     scroller: ".drawer-content",
     trigger: ".dove",
-    markers: true,
     scrub: true,
     start: "200px 0",
     end: "500px start",
@@ -57,12 +55,113 @@ gsap.to(".simon", {
   scrollTrigger: {
     scroller: ".drawer-content",
     trigger: ".simon",
-    markers: true,
-    scrub: true,
     start: "500px 0",
     end: "600px start",
   },
   immediateRender: false,
   y: 200,
   alpha: 0,
+});
+gsap.to(".paul", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".paul",
+    markers: "true",
+    scrub: true,
+    start: "100px 0",
+  },
+  x: 300,
+});
+gsap.to(".paul", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".paul",
+    markers: "true",
+    scrub: true,
+    start: "100px 0",
+  },
+  x: 300,
+});
+
+gsap.to(".words5", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".paul",
+    markers: "true",
+    scrub: true,
+    start: "100px 0",
+  },
+  color: "#fff",
+});
+gsap.from(".jesus", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".paul",
+    markers: "true",
+    scrub: true,
+    start: "100px 0",
+  },
+  y: -200,
+  alpha: 0,
+});
+if (window.$store.currentPage === 3) {
+  gsap.to("#bg", {
+    scrollTrigger: {
+      scroller: ".drawer-content",
+      trigger: ".paul",
+      markers: "true",
+      scrub: true,
+      start: "100px 0",
+    },
+    backgroundColor: "#000000",
+  });
+
+  gsap.from("#bg", {
+    scrollTrigger: {
+      scroller: ".drawer-content",
+      trigger: ".paul",
+      markers: "true",
+      scrub: true,
+      start: "800px 0",
+      end: "900px 0",
+    },
+    backgroundColor: "#ffffff00",
+  });
+}
+gsap.to(".jesus", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".paul",
+    markers: "true",
+    scrub: true,
+    start: "400px 0",
+    end: "800px 0",
+  },
+  y: -200,
+  immediateRender: false,
+  alpha: 0,
+});
+gsap.from(".annanias", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".paul",
+    markers: "true",
+    scrub: true,
+    start: "800px 0",
+    end: "900px 0",
+  },
+  x: -200,
+  alpha: 0,
+});
+
+gsap.to(".words6", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".paul",
+    markers: "true",
+    scrub: true,
+    start: "800px 0",
+    end: "900px 0",
+  },
+  color: "#000000",
 });
