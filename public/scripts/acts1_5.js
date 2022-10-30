157eda043e079b5e4f4f00c6f1e4ff4f6f8982e2
ProjectCodeAI <discordpydev@gmail.com>
2022-10-29 19:45:44 -0700

    Finished Acts 1-5 Animation (but pixel values 🤢)

--- public/scripts/acts1_5.js
+++ public/scripts/acts1_5.js
@@ -24,16 +24,17 @@ gsap.from("#chapter-logo", {
   ease: "out",
 });
 
-gsap.from(".wall", {
+/*const wall = gsap.from(".wall", {
   scrollTrigger: {
     scroller: ".drawer-content",
     trigger: ".wall",
   },
   alpha: 0,
-  y: 800,
-  duration: 5,
+  x: -600,
+  duration: 4,
   ease: "out",
 });
+*/
 
 gsap.to(".disciples", {
   scrollTrigger: {
@@ -85,6 +86,90 @@ gsap.from(".words2", {
   x: 600,
 });
 
+gsap.from(".words3", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".words3",
+    scrub: 1,
+    markers: true,
+    pin: true,
+    start: "0% -45%",
+  },
+  x: 1200,
+  duration: 4,
+});
+
+gsap.from(".matthiaswords", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".matthiaswords",
+    scrub: 1,
+    markers: true,
+    start: "0% -75%",
+  },
+  x: 400,
+  duration: 4,
+});
+
+gsap.from(".standup", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".standup",
+    scrub: 1,
+    markers: true,
+    start: "0% -80%",
+  },
+  x: 1100,
+  duration: 4,
+});
+
+gsap.from(".paulpower", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".paulpower",
+    scrub: 1,
+    markers: true,
+    start: "0% -45%",
+  },
+  x: 400,
+  duration: 4,
+});
+
+gsap.from(".MATTHIAS", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".MATTHIAS",
+    scrub: 1,
+    markers: true,
+    start: "0% 100%",
+  },
+  x: 100,
+  duration: 1,
+});
+
+gsap.to(".wall", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".wall",
+    scrub: true,
+    markers: true,
+    start: "60% 0%",
+  },
+  x: -700,
+});
+
+gsap.to(".matthias", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".matthias",
+    markers: true,
+    pin: ".matthias",
+    start: "50% top",
+    end: "800px start",
+    pinSpacing: "false",
+  },
+});
+
 gsap.to(".disciples", {
   scrollTrigger: {
     scroller: ".drawer-content",
@@ -95,4 +180,60 @@ gsap.to(".disciples", {
     end: "1000px start",
     pinSpacing: "false",
   },
+});
+
+gsap.to("#lame", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: "#lame",
+    markers: true,
+    start: "0% 50%",
+  },
+  duration: 3,
+  autoAlpha: 0,
+});
+
+gsap.to("#healedlame", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: "#healedlame",
+    markers: true,
+    start: "0% 50%",
+  },
+  duration: 2,
+  scale: 1.2,
+  rotation: 360,
+});
+
+gsap.to(".annaniaslie", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".annaniaslie",
+    markers: true,
+    start: "0% 25%",
+  },
+  duration: 3,
+  autoAlpha: 1,
+});
+
+gsap.to(".annanias", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".annanias",
+    markers: true,
+    start: "0% 75%",
+  },
+  duration: 3,
+  autoAlpha: 0,
+});
+
+gsap.to(".wife", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".wife",
+    markers: true,
+    start: "0% 75%",
+  },
+  duration: 3,
+  autoAlpha: 0,
 });
