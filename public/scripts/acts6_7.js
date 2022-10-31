8e3e5dd3c1a2fcb34723d8f420fee867150f35ed
Bryan Hu <bryan.hu.2020@gmail.com>
2022-10-30 17:15:50 -0700

    Amazing progress

--- public/scripts/acts6_7.js
+++ public/scripts/acts6_7.js
@@ -18,6 +18,7 @@ gsap.to(".pinme", {
 let rockContacts = [];
 function animateRock(number, start, end) {
   rockContacts.push(end);
+
   gsap.to(`#rock-${number}`, {
     motionPath: `#rock-path-${number}`,
     scrollTrigger: {
@@ -26,7 +27,7 @@ function animateRock(number, start, end) {
       start: `${start}px 0px`,
       scrub: true,
       end: `${end}px`,
-      markers: { startColor: "blue", endColor: "purple", fontSize: "12px" },
+      // markers: { startColor: "blue", endColor: "purple", fontSize: "12px" },
     },
   });
 }
@@ -34,9 +35,35 @@ function animateRock(number, start, end) {
 animateRock(1, 5, 70);
 animateRock(2, 0, 100);
 animateRock(3, 18, 140);
+animateRock(4, 20, 50);
+animateRock(5, 30, 100);
+animateRock(6, 0, 140);
+animateRock(7, 50, 80);
+animateRock(8, 42, 69);
+
 rockContacts.sort((a, b) => {
   // sort numerically...
   return a - b;
+});
+gsap.to(".stephen-alive", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: "#rocks",
+    start: `${rockContacts[0]}px 0px`,
+    end: `${rockContacts[rockContacts.length - 1]}px`,
+    scrub: true,
+  },
+  opacity: 0,
+});
+gsap.from(".stephen-dead", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: "#rocks",
+    start: `${rockContacts[0]}px 0px`,
+    end: `${rockContacts[rockContacts.length - 1]}px`,
+    scrub: true,
+  },
+  opacity: 0,
 });
 
 for (let i = 0; i < rockContacts.length; i++) {
@@ -48,7 +75,7 @@ for (let i = 0; i < rockContacts.length; i++) {
       start: `${contact - 10}px 0px`,
       end: `+=10px`,
       scrub: true,
-      markers: true,
+      // markers: true,
     },
     immediateRender: i == 0,
     value: 1 - (1 / rockContacts.length) * (i + 1),
