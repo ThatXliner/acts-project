be7182d38d99a6bbc38a615b160f5ebcbbee5087
EvanZhouDev <evanzhouyifan@gmail.com>
2022-11-11 18:02:50 -0800

    💫 Finished the first scene in this section

--- public/scripts/acts10_12.js
+++ public/scripts/acts10_12.js
@@ -4,9 +4,33 @@ gsap.to("#scene1", {
     trigger: "#scene1",
     pin: "#scene1",
     start: "-50px 0",
-    end: "8000px start",
+    end: "2000px start",
     pinSpacing: "false",
   },
+});
+gsap.from(".person1", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".person1",
+    scrub: true,
+    markers: true,
+    start: "1200px 0",
+    end: "1400px 0",
+  },
+  x: -100,
+  opacity: 0,
+});
+gsap.from(".dove", {
+  scrollTrigger: {
+    scroller: ".drawer-content",
+    trigger: ".dove",
+    scrub: true,
+    markers: true,
+    start: "1600px 0",
+    end: "1700px 0",
+  },
+  y: -100,
+  opacity: 0,
 });
 gsap.to(".peter", {
   scrollTrigger: {
