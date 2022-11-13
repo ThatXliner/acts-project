0c9d949e9b40ddcfb7e300ea36807df47b4c29dc
EvanZhouDev <evanzhouyifan@gmail.com>
2022-11-12 18:41:43 -0800

    Reformatted code

--- public/scripts/acts10_12.js
+++ public/scripts/acts10_12.js
@@ -5,8 +5,8 @@ gsap.to("#scene1", {
     pin: "#scene1",
     start: "-50px 0",
     end: "2000px start",
-    pinSpacing: "false",
-  },
+    pinSpacing: "false"
+  }
 });
 gsap.from(".person1", {
   scrollTrigger: {
@@ -15,10 +15,10 @@ gsap.from(".person1", {
     scrub: true,
     markers: true,
     start: "1200px 0",
-    end: "1400px 0",
+    end: "1400px 0"
   },
   x: -100,
-  opacity: 0,
+  opacity: 0
 });
 gsap.from(".dove", {
   scrollTrigger: {
@@ -27,10 +27,10 @@ gsap.from(".dove", {
     scrub: true,
     markers: true,
     start: "1600px 0",
-    end: "1700px 0",
+    end: "1700px 0"
   },
   y: -100,
-  opacity: 0,
+  opacity: 0
 });
 gsap.to(".peter", {
   scrollTrigger: {
@@ -39,10 +39,10 @@ gsap.to(".peter", {
     scrub: true,
     markers: { startColor: "green", endColor: "red", fontSize: "12px" },
     start: "400px 0px",
-    end: "1000px 0px",
+    end: "1000px 0px"
   },
   ease: "out",
-  x: 500,
+  x: 500
 });
 gsap.from(".sheet", {
   scrollTrigger: {
@@ -51,11 +51,11 @@ gsap.from(".sheet", {
     scrub: true,
     markers: { startColor: "green", endColor: "red", fontSize: "12px" },
     start: "400px 0px",
-    end: "1000px 0px",
+    end: "1000px 0px"
   },
   ease: "out",
   opacity: 0,
-  y: -500,
+  y: -500
 });
 gsap.to(".sheet", {
   scrollTrigger: {
@@ -64,10 +64,10 @@ gsap.to(".sheet", {
     scrub: true,
     markers: { startColor: "green", endColor: "red", fontSize: "12px" },
     start: "1200px 0px",
-    end: "2000px 0px",
+    end: "2000px 0px"
   },
   immediateRender: false,
   ease: "out",
   opacity: 0,
-  y: -500,
+  y: -500
 });
